184a42468c76450bb510c142bf7777db905cd882
AbisheakJacob <abisheakjacob0032@gmail.com>
2023-10-17 12:20:00 +0530

    commit

--- jacobtools/__init__.py
+++ jacobtools/__init__.py
@@ -2,7 +2,7 @@
 # in stead of from ppack.functions import average
 
 from .decorators import singleton
-from .db import createdb, dropdb, listdb, createtb, readtb, listtb
+from .db import infodb, listtb
 from .it import combo
 from .pa import fheader
 
