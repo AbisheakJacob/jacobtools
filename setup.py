1d90ae8bce0f5e01a97f5f5309cf11ccbb0bd312
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2024-01-07 00:59:29 +0530

    changes have been made

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/AbisheakJacob/jacobtools',
     project_urls={
-        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
+        "Bug Tracker": "https://github.com/Abisheakjacob/jacobtools/issues"
     },
     license='MIT',
     packages=['jacobtools'],
