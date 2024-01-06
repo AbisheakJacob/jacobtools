80125e3cc258f65c4baf68b72d57b9668781aa52
Abisheak Jacob J <abisheakjacob0032@gmail.com>
2024-01-07 00:56:15 +0530

    changes have been made to the database module

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='jacobtools',
-    version='0.0.10',
+    version='0.0.11',
     author='Abisheak Jacob J',
     author_email='abisheakjacob0032@gmail.com',
     description='my package',
