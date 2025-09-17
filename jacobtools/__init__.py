b64a35123ddf509a86200fb8a184af92cead9b86
Abisheak Jacob J <abisheakjacob.j@actived-impactanalytics.com>
2025-09-17 13:38:11 +0530

    added fn: generate create table sql

--- jacobtools/__init__.py
+++ jacobtools/__init__.py
@@ -2,7 +2,9 @@
 
 from .import config
 from .preprocessing import drop_columns_by_schema
+from .sql_generation import generate_create_table_sql
 
 __all__ =[
-    drop_columns_by_schema
+    drop_columns_by_schema,
+    generate_create_table_sql
 ]
---
+++ jacobtools/sql_generation/postgres/__init__.py
@@ -0,0 +1,6 @@
+from .generate_create_table_sql import generate_create_table_sql, infer_pg_type
+
+__all__ = [
+    generate_create_table_sql,
+    infer_pg_type
+]
